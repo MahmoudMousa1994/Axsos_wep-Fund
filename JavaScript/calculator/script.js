e9f54813720581ff4f99e7c1da6979aba4ae7248
mhd <mahmoud.shuman@outlook.com>
2025-06-28 23:59:20 +0300

    js calculator

---
+++ JavaScript/calculator/script.js
@@ -0,0 +1,40 @@
+let displayVariable = document.querySelector("#display");
+let current = "" ;
+let opperate = "";
+let temp ="";
+function press(index){
+    current+= index
+    displayVariable.textContent = current;
+}
+function setOP(op){
+    temp = current
+    opperate = op
+    current =""
+}
+function calculate(){
+    let num1 = parseFloat(temp);
+    let num2 = parseFloat(current);
+    if (opperate ==='/'){
+        let equal = num1/num2
+    displayVariable.textContent = equal;
+    }else if(opperate ==='*')
+    {
+        let equal = num1*num2
+    displayVariable.textContent = equal;
+    }else if(opperate ==='-')
+    {
+        let equal = num1-num2
+    displayVariable.textContent = equal;
+    }else if(opperate ==='+')
+    {
+        let equal = num1+num2
+    displayVariable.textContent = equal;
+    }
+    
+}
+function clr(){
+    current="";
+    opperate="";
+    temp = "";
+    displayVariable.textContent = "0";
+}
